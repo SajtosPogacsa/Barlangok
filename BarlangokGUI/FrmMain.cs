ad722e3f627330a4ce13eb1b8411fc3f218038d7
Busai Dániel <busai.dani@gmail.com>
2025-01-14 12:15:48 +0100

    Kész

--- BarlangokGUI/FrmMain.cs
+++ BarlangokGUI/FrmMain.cs
@@ -7,18 +7,49 @@ namespace BarlangokGUI
         const string SOLDIR = "H:\\repos\\Barlangok\\src";
 
         List<Barlang> barlangok = new();
-
+        Barlang b;
         public FrmMain()
         {
             InitializeComponent();
             this.Load += FrmMainLoad;
             searchBtn.Click += SearchBtnClick;
+            saveBtn.Click += SaveBtnClick;
+        }
+
+        private void ResetField()
+        {
+            idTbx.Text = "";
+            nameLbl.Text = $"A barlang neve:";
+            lengthTbx.Text = $"";
+            depthTbx.Text = $"";
+            lengthTbx.Enabled = false;
+            depthTbx.Enabled = false;
+            saveBtn.Enabled = false;
+        }
+
+        private void SaveBtnClick(object? sender, EventArgs e)
+        {
+            int depth = int.Parse(depthTbx.Text);
+            int length = int.Parse(lengthTbx.Text);
+            int id = int.Parse(idTbx.Text);
+
+            if ( depth < b.Melyseg || length < b.Hossz)
+            {
+                MessageBox.Show("A mélység és a hossz nem lehet kisebb, mint az eredeti érték!");
+            }
+            else
+            {
+                b.Hossz = length;
+                b.Melyseg = depth;
+            }
+            ResetField();
         }
 
         private void SearchBtnClick(object? sender, EventArgs e)
         {
             string id = idTbx.Text;
-            Barlang b = barlangok.Find(x => x.Azon == int.TryParse(id, out idInt));
+            int idInt = int.Parse(id);
+            b = barlangok.Find(x => x.Azon == idInt);
             
             if (b == null || id.Length == 0)
             {
@@ -26,7 +57,16 @@ namespace BarlangokGUI
                     text: "Ezzel az azonosítóval nem létezik barlang!",
                     caption: "Hiba!");
 
-                idTbx.Text = "";
+                ResetField();
+            }
+            else
+            {
+                nameLbl.Text = $"A barlang neve: {b.Nev}";
+                lengthTbx.Text = $"{b.Hossz}";
+                depthTbx.Text = $"{b.Melyseg}";
+                lengthTbx.Enabled = true;
+                depthTbx.Enabled = true;
+                saveBtn.Enabled = true;
             }
 
         }
